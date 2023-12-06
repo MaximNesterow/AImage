4a7ff8f1bf4a7b882bf76a886ec86daf2bddabfb
MaximNesterow <149317261+MaximNesterow@users.noreply.github.com>
2023-12-07 00:27:32 +0700

    Update aimage.py

--- aimage.py
+++ aimage.py
@@ -1,11 +1,9 @@
-import pixellib
-import tensorflow
 from pixellib.instance import instance_segmentation
 def aimage():
     predmet=instance_segmentation()
-    predmet.load_model()
+    predmet.load_model("")
+    predmet.segmentImage(image_path="", show_bboxes=True, output_image_name="OK")
 def poisk():
     aimage()
-if __name__=='__name__':
+if __name__ == '__main__':
     poisk()
-print("hello")
